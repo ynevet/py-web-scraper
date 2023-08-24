80f86eef8e9f9b8c74889104dbc0d2fc52752ea8
Yair Nevet <ynevet@gmail.com>
2023-08-25 01:55:13 +0300

    refactor

--- main.py
+++ main.py
@@ -4,43 +4,58 @@ import pandas as pd
 import html
 import datetime
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
 
-driver = webdriver.Chrome(options=options)
+def initialize_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
 
-products = []
-prices = []
-currencies = []
-ratings = []
-
-driver.get("https://uae.sharafdg.com/c/computing/laptops/macbooks/")
-
-content = driver.page_source
-soup = BeautifulSoup(content, features="html.parser")
-for a in soup.findAll('a', href=True, attrs={'class': 'ratio-box product-link'}):
-    name = a.find('h4', attrs={'class': 'name'})
-    price = a.find('div', attrs={'class': 'price'})
-    currency, amount = html.unescape(price.text).split()
-    rating = a.find('span', attrs={'class': 'product-rating-count'})
-    prices.append(amount.strip())
-    currencies.append(currency.strip())
-    products.append(name.text.strip())
-    ratings.append(rating.text.strip().strip('()'))
+    return webdriver.Chrome(options=options)
 
 
-df = pd.DataFrame(
-    {'Product Name': products, 'Price': prices, 'Currency': currency, 'Rating': ratings})
+def get_product_data(driver, url):
+    driver.get(url)
+    content = driver.page_source
+    soup = BeautifulSoup(content, features="html.parser")
 
-current_date = datetime.date.today()
-current_year = current_date.year
-current_month = current_date.month
+    products, prices, currencies, ratings = [], [], [], []
 
-df['year'] = current_year
-df['month'] = current_month
+    for a in soup.findAll('a', href=True, attrs={'class': 'ratio-box product-link'}):
+        name = a.find('h4', attrs={'class': 'name'})
+        price = a.find('div', attrs={'class': 'price'})
+        currency, amount = html.unescape(price.text).split()
+        rating = a.find('span', attrs={'class': 'product-rating-count'})
 
-df.to_csv('products.csv', index=False, encoding='utf-8')
-df.to_parquet('data.parquet', engine='pyarrow',
-              partition_cols=['year', 'month'])
+        prices.append(amount.strip())
+        currencies.append(currency.strip())
+        products.append(name.text.strip())
+        ratings.append(rating.text.strip().strip('()'))
+
+    return products, prices, currencies, ratings
+
+
+def main():
+    url = "https://uae.sharafdg.com/c/computing/laptops/macbooks/"
+
+    with initialize_driver() as driver:
+        products, prices, currencies, ratings = get_product_data(driver, url)
+
+        df = pd.DataFrame({
+            'Product Name': products,
+            'Price': prices,
+            'Currency': currencies,
+            'Rating': ratings
+        })
+
+        current_date = datetime.date.today()
+        df['year'] = current_date.year
+        df['month'] = current_date.month
+
+        df.to_csv('products.csv', index=False, encoding='utf-8')
+        df.to_parquet('data.parquet', engine='pyarrow',
+                      partition_cols=['year', 'month'])
+
+
+if __name__ == "__main__":
+    main()
