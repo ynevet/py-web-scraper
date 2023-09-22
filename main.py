c9e101e81591cba3ff08e8f73d98bcb55fd2f0f8
Yair Nevet <ynevet@gmail.com>
2023-09-22 13:51:22 +0300

    update headless flag

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import datetime
 
 def initialize_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
+    options.add_argument('--headless=new')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
 
